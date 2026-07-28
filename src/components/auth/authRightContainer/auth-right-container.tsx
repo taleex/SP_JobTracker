bb1507fbc284c7dcefd950241ec61ff766a5783a
Taleex <matosjax@gmail.com>
2026-07-28 16:46:23 +0100

    Ttitle added

--- src/components/auth/authRightContainer/auth-right-container.tsx
+++ src/components/auth/authRightContainer/auth-right-container.tsx
@@ -3,6 +3,7 @@ import { AuthForm } from "./auth-form-index";
 export default function AuthRightContainer() {
   return (
     <section className="auth-right">
+      <h1 className="auth-form-title">Create Account!</h1>
       <AuthForm />
     </section>
   );
