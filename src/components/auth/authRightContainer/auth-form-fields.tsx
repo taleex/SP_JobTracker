5ad2f3823c8e41e4eea4ed8e979c3e5dd6be5857
Taleex <matosjax@gmail.com>
2026-07-31 18:41:00 +0100

    auth starting action server

--- src/components/auth/authRightContainer/auth-form-fields.tsx
+++ src/components/auth/authRightContainer/auth-form-fields.tsx
@@ -7,43 +7,48 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
+import Link from "next/link";
 
 export function AuthForm() {
   return (
-    <FieldGroup>
-      <Field>
-        <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
-        <Input id="fieldgroup-name" placeholder="Jordan Lee" />
-      </Field>
+    <form>
+      <FieldGroup>
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-name">Name</FieldLabel>
+          <Input id="fieldgroup-name" placeholder="Jordan Lee" />
+        </Field>
 
-      <Field>
-        <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
-        <Input
-          id="fieldgroup-email"
-          type="email"
-          placeholder="name@example.com"
-        />
+        <Field>
+          <FieldLabel htmlFor="fieldgroup-email">Email</FieldLabel>
+          <Input
+            id="fieldgroup-email"
+            type="email"
+            placeholder="name@example.com"
+          />
 
-        {/* Checkbox - Remember me */}
-        <FieldGroup className="mx-auto w-56">
-          <Field orientation="horizontal">
-            <Checkbox
-              id="terms-checkbox-invalid"
-              name="terms-checkbox-invalid"
-            />
-            <FieldLabel htmlFor="terms-checkbox-invalid">
-              remember me?
-            </FieldLabel>
-          </Field>
-        </FieldGroup>
-      </Field>
+          {/* Checkbox - Remember me */}
+          <FieldGroup className="mx-auto w-56">
+            <Field orientation="horizontal">
+              <Checkbox
+                id="terms-checkbox-invalid"
+                name="terms-checkbox-invalid"
+              />
+              <FieldLabel htmlFor="terms-checkbox-invalid">
+                remember me?
+              </FieldLabel>
+            </Field>
+          </FieldGroup>
+        </Field>
 
-      <Field orientation="horizontal">
-        <Button type="reset" variant="outline">
-          Reset
-        </Button>
-        <Button type="submit">Submit</Button>
-      </Field>
-    </FieldGroup>
+        <Field orientation="horizontal">
+          <Link href="/">
+            <Button type="reset" variant="outline">
+              back
+            </Button>
+          </Link>
+          <Button type="submit">Submit</Button>
+        </Field>
+      </FieldGroup>
+    </form>
   );
 }
